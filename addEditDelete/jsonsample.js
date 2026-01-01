1bddd221c5ae7afccb405474921ee544efee72a7
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2026-01-01 10:55:01 +0330

    Add files via upload

---
+++ addEditDelete/jsonsample.js
@@ -0,0 +1,9 @@
+ let jsonData = `
+        [
+            { "fname":"Omid", "lname":"Lak", "MaritalStatus": true },
+            { "fname":"Mohammad", "lname":"Naderi", "MaritalStatus": false },
+            { "fname":"Ali", "lname":"Nasiri", "MaritalStatus": true },
+            { "fname":"Nader", "lname":"Hosseini", "MaritalStatus": false }
+        ]`;
+
+        let names = JSON.parse(jsonData);
